7d62c8ac87f824309730414d4f3eabf9576180f2
Daniel Draper <daniel.draper@student.kit.edu>
2017-02-27 18:10:30 +0100

    added test script to runEval

--- runEvalLength.sh
+++ runEvalLength.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tail -n 33 testlog > evalResults/testlog$1
+tail -n 33 testlogBare > evalResults/testlogBare$1
 tail -n 33 testlogBothFilters > evalResults/testlogBothFilters$1
 tail -n 33 testlogFilter > evalResults/testlogFilter$1
 tail -n 33 testlogVariance > evalResults/testlogVariance$1
@@ -8,6 +9,8 @@ tail -n 9 testlogDEEN > evalResults/testlogDEEN$1
 
 length/prepBase.sh testlog
 length/calcStat.sh base$1
+length/prepBase.sh testlogBare
+length/calcStat.sh bare$1
 length/prepBase.sh testlogDEEN
 length/calcStat.sh DEEN$1
 length/prepNoBase.sh testlogFilter
